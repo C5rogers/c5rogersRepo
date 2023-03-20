ae629b70ad5a66de3910259fda3b181e7d8b3ce1
c5rogers <natenaelhaylemariam@gmail.com>
2023-03-20 13:25:09 -0400

    my reaction timer game

---
+++ working with Vue js/project4/reaction-timer/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Natty Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @reaction-time="endGame" />
+  <Results :result="score" v-if="showResults" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results,
+  },
+  data(){
+    return {
+      isPlaying:false,
+      delay:null,
+      score:null,
+      showResults:false,
+    }
+  },
+  methods:{
+    start(){
+      this.delay=2000+Math.random()*5000
+      this.isPlaying=true
+      this.showResults=false
+    },
+    endGame(reactionTime){
+      this.score=reactionTime
+      this.isPlaying=false
+      this.showResults=true
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button{
+  padding: .5em .6em;
+  color: aliceblue;
+  background-color:  #144116;
+  border: none;
+  width: 5em;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1em;
+}
+button[desabled]{
+  opacity: 10%;
+  cursor: not-allowed;
+}
+</style>
---
+++ working with Vue js/project4/reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+<p>Reaction time - {{ result }} ms</p>
+<p class="rank">{{ rank }}</p>
+</template>
+<script>
+export default{
+    name:'Results',
+    props:['result'],
+    data(){
+        return{
+            rank:null,
+        }
+    },
+    mounted(){
+        if(this.result<250){
+            this.rank='Ninja Fingers'
+        }else if(this.result<400){
+            this.rank='rapid Reflexes'
+        }else{
+            this.rank='Snall pace...'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rank{
+    font-size: 1.4em;
+    color:  #144116;
+    font-weight: bolder;
+}
+</style>
